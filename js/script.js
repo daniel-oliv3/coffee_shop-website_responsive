37e8b4534924053b8438b43787e6a15d7e1933e1
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-01-19 15:16:52 -0300

    Login

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
+let loginForm = document.querySelector('.login-form');
+
+document.querySelector('#login-btn').onclick = () => {
+    loginForm.classList.add('active');
+}
+
 //Sass -> sass --watch ./scss/style.scss:./css/style.css
